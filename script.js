ee67b2701825bc9ab7b80ee9e8d8a985976e4fd4
Gabriele-Scaloni <88436304+Gabriele-Scaloni@users.noreply.github.com>
2021-11-17 17:54:51 +0100

    da mettere il timer

--- script.js
+++ script.js
@@ -10,159 +10,127 @@ messaggi relativi al contatto attivo all’interno del pannello della conversazi
 */
 
 var app = new Vue({
-    el: '#app',
-    data: {
-      contactActive:0,
-contacts: [
-    {
-    name: 'Michele',
-    avatar: 'img/avatar_1.jpg',
-    visible: true,
-    messages: [
-        {
-        date: '10/01/2020 15:30:55',
-        text: 'Hai portato a spasso il cane?',
-        status: 'sent'
-        },
-        {
-        date: '10/01/2020 15:50:00',
-        text: 'Ricordati di dargli da mangiare',
-        status: 'sent'
-        },
-        {
-        date: '10/01/2020 16:15:22',
-        text: 'Tutto fatto!',
-        status: 'received'
-        }
-        ],
-        },
-    {
-    name: 'Fabio',
-    avatar: 'img/avatar_2.jpg',
-    visible: true,
-    messages: [
-        {
-        date: '20/03/2020 16:30:00',
-        text: 'Ciao come stai?',
-        status: 'sent'
-        },
-        {
-        date: '20/03/2020 16:30:55',
-        text: 'Bene grazie! Stasera ci vediamo?',
-        status: 'received'
-        },
-        {
-        date: '20/03/2020 16:35:00',
-        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-        status: 'sent'
-        }
-        ],
-        },
-    {
-    name: 'Samuele',
-    avatar: 'img/avatar_3.jpg',
-    visible: true,
-    messages: [
-        {
-        date: '28/03/2020 10:10:40',
-        text: 'La Marianna va in campagna',
-        status: 'received'
-        },
-        {
-        date: '28/03/2020 10:20:10',
-        text: 'Sicuro di non aver sbagliato chat?',
-        status: 'sent'
-        },
-        {
-        date: '28/03/2020 16:15:22',
-        text: 'Ah scusa!',
-        status: 'received'
-        }
-        ],
-        },
-    {
-    name: 'Luis',
-    avatar: 'img/avatar_4.jpg',
-    visible: true,
-    messages: [
-        {
-        date: '10/01/2020 15:30:55',
-        text: 'Lo sai che ha aperto una nuova pizzeria?',
-        status: 'sent'
-        },
-        {
-        date: '10/01/2020 15:50:00',
-        text: 'Si, ma preferirei andare al cinema',
-        status: 'received'
-        }
-    ],
-   
-   },
-  ],  
-  newMessage: [
+  el: '#app',
+  data: {
+    contactActive: 0,
+    contacts: [
       {
+        name: 'Michele',
+        avatar: 'img/avatar_1.jpg',
+        visible: true,
+        messages: [
+          {
+            date: '10/01/2020 15:30:55',
+            text: 'Hai portato a spasso il cane?',
+            status: 'sent'
+          },
+          {
+            date: '10/01/2020 15:50:00',
+            text: 'Ricordati di dargli da mangiare',
+            status: 'sent'
+          },
+          {
+            date: '10/01/2020 16:15:22',
+            text: 'Tutto fatto!',
+            status: 'received'
+          }
+        ],
+      },
+      {
+        name: 'Fabio',
+        avatar: 'img/avatar_2.jpg',
+        visible: true,
+        messages: [
+          {
+            date: '20/03/2020 16:30:00',
+            text: 'Ciao come stai?',
+            status: 'sent'
+          },
+          {
+            date: '20/03/2020 16:30:55',
+            text: 'Bene grazie! Stasera ci vediamo?',
+            status: 'received'
+          },
+          {
+            date: '20/03/2020 16:35:00',
+            text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+            status: 'sent'
+          }
+        ],
+      },
+      {
+        name: 'Samuele',
+        avatar: 'img/avatar_3.jpg',
+        visible: true,
+        messages: [
+          {
+            date: '28/03/2020 10:10:40',
+            text: 'La Marianna va in campagna',
+            status: 'received'
+          },
+          {
+            date: '28/03/2020 10:20:10',
+            text: 'Sicuro di non aver sbagliato chat?',
+            status: 'sent'
+          },
+          {
+            date: '28/03/2020 16:15:22',
+            text: 'Ah scusa!',
+            status: 'received'
+          }
+        ],
+      },
+      {
+        name: 'Luis',
+        avatar: 'img/avatar_4.jpg',
+        visible: true,
+        messages: [
+          {
+            date: '10/01/2020 15:30:55',
+            text: 'Lo sai che ha aperto una nuova pizzeria?',
+            status: 'sent'
+          },
+          {
+            date: '10/01/2020 15:50:00',
+            text: 'Si, ma preferirei andare al cinema',
+            status: 'received'
+          }
+        ],
+
+      },
+    ],
+    newMessage: {
       date: '',
       text: '',
       status: 'sent'
-    }
-  ],
-  newMessageRecived: [
-    {
-    date: '',
-    text: '',
-    status: 'received'
+    },
+    newMessageRecived: {
+      date: '',
+      text: 'ok!',
+      status: 'received'
+    },
   },
-],
-  
-},
-methods: {
-  changeUser(index) {
-    this.contactActive = index;
-  },
+  methods: {
+    changeUser(index) {
+      this.contactActive = index;
+    },
+    //scrivo un nuovo messaggio
+    addMessage() {
+      this.contacts[this.contactActive].messages.push(this.newMessage);
+      this.newMessage = {
+        date: '',
+        text: '',
+        status: 'sent'
+      }
+    },
 
-//scrivo un nuovo messaggio
- addMessage  (){
-  this.contacts.push(this.newMessage); 
-     this.newMessage = {
-     date: '',
-     text:'',
-     status: 'sent'
- }   
-}, 
-//ricevo un nuovo messaggio
-/* recivedMessage  (){
-  this.contacts.push(this.newMessageRecived); 
-     this.newMessageRecived = {
-     date: '',
-     text:'',
-     status: 'received'
- }   
-} */
+  }
+})
 
 
-}
-}) 
-/*     addMessage  (index){
-    this.messages.push(this.newMessage); 
-       this.newMessage = {
-       text:'',
-       status: 'sent'
-   },   
-}
-  */
 
-/*  
-  messaggioVisible (index){}
-  let contatto = this.contacts[this.contactActive];
-  let messaggio = 'message' + contatto.messaggio
-  return messaggio;
-
-},  */
- 
-
-
- /* function ricerca() {
-    var input; 
+/* function ricerca() {
+    var input;
     var filtro;
     var lista;
     var voci;
@@ -182,13 +150,5 @@ methods: {
           voci[i].style.display = "none";
         }
       }
-    }  */
+    }   */
 
-/* getNow() {
-                const today = new Date();
-                const date = today.getDate() +'/'+(today.getMonth()+1)+'/'+ today.getFullYear();
-                const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-                const dateTime = date +' '+ time;
-                this.dateNow = dateTime;
-            }
- */
